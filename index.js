b967d56c8afa6c2666b319f3fcc74ab71defa63d
Bhavana <bhavana191919@gmail.com>
2025-05-18 21:21:43 +0530

    initial commit

--- index.js
+++ index.js
@@ -37,13 +37,66 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+import { stagger } from 'animejs';
 
+const functionValue = stagger(value, parameters);
+
+import { createtimeline, stagger } from 'animejs';
+
+const tl = createTimeline({
+  loop: true,
+  alternate: true,
+})
+.add('.row:nth-child(1) .square', {
+  scale: 0,
+  delay: stagger(50, { from: 8 }),
+})
+.add('.row:nth-child(2) .square', {
+  scale: 0,
+  delay: stagger(50, { from: 'first' }),
+})
+.add('.row:nth-child(3) .square', {
+  scale: 0,
+  delay: stagger(50, { from: 'center' }),
+})
+.add('.row:nth-child(4) .square', {
+  scale: 0,
+  delay: stagger(50, { from: 'last' }),
+});
+import { animate, stagger } from 'animejs';
+
+const $squares = utils.$('.square');
+
+function animateGrid() {
+  animate($squares, {
+    scale: [
+      { to: [0, 1.25] },
+      { to: 0 }
+    ],
+    boxShadow: [
+      { to: '0 0 1rem 0 currentColor' },
+      { to: '0 0 0rem 0 currentColor' }
+    ],
+    delay: stagger(100, {
+      grid: [11, 4],
+      from: utils.random(0, 11 * 4)
+    }),
+    onComplete: animateGrid
+  });
+}
+
+animateGrid();
 import { animate, stagger } from 'animejs';
 
 animate('.square', {
-  x: '17rem',
-  delay: stagger(100),
-  duration: stagger(200, { start: 500 }),
-  loop: true,
-  alternate: true
+  boxShadow: [
+    { to: stagger([1, .25], {
+        modifier: v => `0 0 ${v * 30}px ${v * 20}px currentColor`,
+        from: 'center'
+      })
+    },
+    { to: 0 },
+  ],
+  delay: stagger(100, { from: 'center' }),
+  loop: true
 });
